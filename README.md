519f4d7cf0b58cf39e7024082119392a885cf5a6
Paul Copley <pkcopley@gmail.com>
2018-06-29 09:55:15 +0100

    Update README.md with pseudo-classical and classes

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Your task is to use Object orientated programming to to model a boat that will load and unload cargo.
 
+## Task 1
+
+Using the pseudo classical style, complete the Boat function so that an instance of Boat has the listed properties.
+
+## Task 2 
+
+Add each of the listed methods to the Boat prototype. 
+
+## Task 3
+
+Refactor your implementation to use ES6 classes instead of the pseudo-classical style.
+
+Create your class in the boatClass.js file and change the require in the tests to check your implementation
+
+
 A test suite has been provided for you to check your implementation.
 
 ## Properties
--- boat.js
+++ boat.js
@@ -1,25 +1,3 @@
-class Boat {
-  constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    this.bearing = 'north';
-    this.cargo = {};
-  }
-  getBearing() {
-    return this.bearing;
-  }
-  turn(bearing) {
-    this.bearing = bearing;
-  }
-  loadCargo(item) {
-    if (this.cargo[item.name]) this.cargo[item.name] += item.quantity;
-    else this.cargo[item.name] = item.quantity;
-  }
-  unLoad(quantity, name) {
-    if (this.cargo[name] < quantity) return 'not enough cargo';
-    this.cargo[name] -= quantity;
-    return `${quantity} ${name} were unloaded from ${this.name}`;
-  }
-}
+function Boat() {}
 
 module.exports = { Boat };
